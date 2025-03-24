15ef1cf5ef8a458913a281ec43e7a7c9b9dcc0a3
Pedro Silva <pedro@dimensiva.com.br>
2025-03-24 19:39:44 -0300

    Primeiro commit

---
+++ src/test/java/br/com/solipy/loja/LojaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solipy.loja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
